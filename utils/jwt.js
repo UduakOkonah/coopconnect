d563e0e232c9906ad2d03badf63912f6a0eec96c
metalpadawan <emmanuelndem9@gmail.com>
2025-10-18 00:35:09 +0100

    back a step

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,11 +1,14 @@
+// utils/jwt.js
 const jwt = require("jsonwebtoken");
 
-const signUser = (user) => {
-  return jwt.sign(
-    { id: user._id, email: user.email },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" }
-  );
+const signToken = (user) => {
+  const payload = {
+    id: user._id,
+    email: user.email,
+    name: user.name || user.displayName || "User",
+  };
+
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
-module.exports = { signUser };
+module.exports = { signToken };
