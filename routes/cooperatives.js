9d10d423de40a325701d825f3fa44afbdaff458b
metalpadawan <emmanuelndem9@gmail.com>
2025-10-17 18:12:06 +0100

    error fixed

--- routes/cooperatives.js
+++ routes/cooperatives.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const cooperatives = require("../controllers/cooperativesontroller");
+const cooperatives = require("../controllers/cooperativesController");
 const { protect } = require("../middleware/auth");
 
 /**
